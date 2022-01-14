2869782e0cf097648b3076985523ac46fc99ffef
doitlock <doitlock@gmail.com>
2022-01-14 23:38:18 +0900

    2022년 포트폴리오 재정비 시작!

---
+++ sub/sub/kakaobank/sub/js/default.js
@@ -0,0 +1,20 @@
+$('.intro_img_slide').slick({
+	dots: false,
+	infinite: true,
+	fade: true,
+	cssEase: 'linear',
+	autoplay: true,
+	autoplaySpeed: 2000,
+	arrows: false
+});
+
+$('.video_slide').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	arrows: true,
+	dots: true,
+	autoplay: true,
+	autoplaySpeed: 2000
+});
+
