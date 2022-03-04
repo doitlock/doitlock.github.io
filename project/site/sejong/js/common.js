a03bea4e8c8b5f1deaac2b8d620de8d6a80ab758
doitlock <doitlock@gmail.com>
2022-03-04 15:16:22 +0900

    포트폴리오 세종복지다옴 클론코딩, 바닐라 자바스크립트 미니프로젝트

--- project/site/sejong/js/common.js
+++ project/site/sejong/js/common.js
@@ -1,12 +1,15 @@
 // common script
 'use strict'
 
+const header = document.getElementById('header');
+
 //부드러운스크롤링크이동
 function go_scroll(id_name)	{
-	var top_offs = $('#'+id_name).offset().top;
+	let top_offs = $('#'+id_name).offset().top;
 	$( 'html, body' ).stop().animate( { scrollTop : top_offs } );
-}
+};
 
+//header에 hover하면 active클래스 추가
 
 
 
