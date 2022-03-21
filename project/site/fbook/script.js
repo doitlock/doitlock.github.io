842b4ec2d8e843d842ee033cb27efb9ee79e2d42
doitlock <doitlock@gmail.com>
2022-03-21 16:15:51 +0900

    개인포폴 사이트 다크버전 스크립트 수정

--- project/site/fbook/script.js
+++ project/site/fbook/script.js
@@ -12,21 +12,21 @@ darkBtn.onclick = function() {
 	darkBtn.classList.toggle('dark-btn-on');
 	document.body.classList.toggle('dark-theme');
 
-	if (localStorage.getItem('theme') == 'light') {
-		localStorage.setItem('theme', 'dark');
+	if (localStorage.getItem('bgmode') == 'light') {
+		localStorage.setItem('bgmode', 'dark');
 	} else {
-		localStorage.setItem('theme', 'light');
+		localStorage.setItem('bgmode', 'light');
 	}
 }
 
-if (localStorage.getItem('theme') == 'light') {
+if (localStorage.getItem('bgmode') == 'light') {
 	darkBtn.classList.remove('dark-btn-on');
 	document.body.classList.remove('dark-theme');
-} else if(localStorage.getItem('theme') == 'dark') {
+} else if(localStorage.getItem('bgmode') == 'dark') {
 	darkBtn.classList.add('dark-btn-on');
 	document.body.classList.add('dark-theme');
 } else {
-	localStorage.setItem('theme', 'light');
+	localStorage.setItem('bgmode', 'light');
 }
 
 //셋,겟타임에 들어가는 theme등 텍스트는 걍 아무거나 써도 됨. 메소드 아님
