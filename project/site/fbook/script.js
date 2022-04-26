9268d8db93cbcb095ef04d130edfc7d35187cfde
doitlock <doitlock@gmail.com>
2022-04-26 22:41:20 +0900

    포트폴리오 작업 업데이트

--- project/site/fbook/script.js
+++ project/site/fbook/script.js
@@ -1,7 +1,9 @@
 'use strict'
 
-let settingsMenu = document.querySelector('.settings-menu');
-let darkBtn = document.getElementById('dark-btn');
+const settingsMenu = document.querySelector('.settings-menu');
+const darkBtn = document.getElementById('dark-btn');
+const clock = document.getElementById('clock');
+const todayDate = document.getElementById('today-date');
 
 function settingsMenuToggle() {
 	settingsMenu.classList.toggle('settings-menu-height');
@@ -19,6 +21,7 @@ darkBtn.onclick = function() {
 	}
 }
 
+//set,get item에 들어가는 theme등 텍스트는 걍 아무거나 써도 된다.
 if (localStorage.getItem('bgmode') == 'light') {
 	darkBtn.classList.remove('dark-btn-on');
 	document.body.classList.remove('dark-theme');
@@ -29,4 +32,24 @@ if (localStorage.getItem('bgmode') == 'light') {
 	localStorage.setItem('bgmode', 'light');
 }
 
-//셋,겟타임에 들어가는 theme등 텍스트는 걍 아무거나 써도 됨. 메소드 아님
+//현재 시간
+function getClock() {
+	const date = new Date();
+	const hours = String(date.getHours()).padStart(2, '0');
+	const minutes = String(date.getMinutes()).padStart(2, '0');
+	const seconds = String(date.getSeconds()).padStart(2, '0');
+	clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();//밑에꺼랑 순서 중요
+setInterval(getClock, 1000); //순서중요
+
+//오늘 날짜
+function getDate() {
+	const date = new Date();
+	let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+	todayDate.innerText = String(date.toLocaleDateString('ko-KR', options));
+}
+
+getDate();
+
