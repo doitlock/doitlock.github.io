7bb24da3cc9ddd32cb828e026d762b5997f43cc7
펜타브리드_홍수기 <doitlock@gmail.com>
2025-04-28 15:09:47 +0900

    feat: 250428 세팅중

--- src/study/js_핵심가이드/program.js
+++
@@ -1,9 +0,0 @@
-document.writeln('Hello, world!')
-
-Function.prototype.method = function (name, func) {
-    this.prototype[name] = func;
-    return this;
-}
-
-// var that = this;  // 주석우리 위한 기호로 /**/ 대신 //를 사용하는 것을 권함
-
